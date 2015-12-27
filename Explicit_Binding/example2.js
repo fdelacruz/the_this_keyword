9e92e6dfffd28c898569a5e220ea29b59413399b
Francisco De La Cruz <helloquico@gmail.com>
2015-12-26 19:54:19 -0500

    Explicit Binding

---
+++ Explicit_Binding/example2.js
@@ -0,0 +1,21 @@
+// - Implicit Binding
+// - Explicit Binding
+// - new Binding
+// - window Binding
+
+// Explicit Binding
+// call, apply, bind
+
+sayName = function (lang1, lang2, lang3) {
+  console.log('My name is ' + this.name + ' and I know ' + lang1 + ', ' + lang2 + ', and ' + lang3);
+};
+
+var stacey = {
+  name: 'Stacey',
+  age: 34
+};
+
+var languages = ['javascript', 'Ruby', 'Python'];
+
+sayName.apply(stacey, languages);
+
---
+++ Explicit_Binding/example3.js
@@ -0,0 +1,21 @@
+// - Implicit Binding
+// - Explicit Binding
+// - new Binding
+// - window Binding
+
+// Explicit Binding
+// call, apply, bind
+
+sayName = function (lang1, lang2, lang3) {
+  console.log('My name is ' + this.name + ' and I know ' + lang1 + ', ' + lang2 + ', and ' + lang3);
+};
+
+var stacey = {
+  name: 'Stacey',
+  age: 34
+};
+
+var languages = ['javascript', 'Ruby', 'Python'];
+
+var newFn = sayName.bind(stacey, languages[0], languages[1], languages[2]);
+newFn();
